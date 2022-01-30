c1efdcda9c092e544ee6957413571a85f4132b37
VitizLaPeste <Lucas.grosjean07@orange.fr>
2022-01-30 12:37:59 +0100

    Credits upgrade

--- code/bits/CreditEntity.cc
+++ code/bits/CreditEntity.cc
@@ -22,6 +22,8 @@ namespace hg {
     gf::Coordinates coords(target);
 
     unsigned titleCharacterSize = coords.getRelativeCharacterSize(0.1f);
+    unsigned subtitleCharacterSize = coords.getRelativeCharacterSize(0.07f);
+    unsigned gitCharacterSize = coords.getRelativeCharacterSize(0.05f);
 
     gf::Text title("Credits", m_font, titleCharacterSize);
     title.setColor(gf::Color::White);
@@ -29,13 +31,49 @@ namespace hg {
     title.setAnchor(gf::Anchor::Center);
     target.draw(title, states);
 
-    unsigned subtitleCharacterSize = coords.getRelativeCharacterSize(0.13f);
+    gf::Text git("", m_font, gitCharacterSize);
+    git.setParagraphWidth(40.0f);
+    git.setString("Github: https://github.com/Hatunruna/ggj2022");
+    git.setColor(gf::Color::White);
+    git.setPosition(coords.getRelativePoint({ 0.5f, 0.7f}));
+    git.setAnchor(gf::Anchor::Center);
 
-    gf::Text subtitle("Hatunruna", m_font, subtitleCharacterSize);
-    subtitle.setColor(gf::Color::White);
-    subtitle.setPosition(coords.getRelativePoint({ 0.5f, 0.5f }));
-    subtitle.setAnchor(gf::Anchor::Center);
-    target.draw(subtitle, states);
+    gf::Text developper("", m_font, subtitleCharacterSize);
+    developper.setParagraphWidth(20.0f);
+    developper.setAlignment(gf::Alignment::Justify);
+    developper.setString("Developper:"
+                         " Ahugeat "
+                         " Jube"
+                         " Magestik "
+                         " Mcvitiz "
+                         " NiiRoZz ");
+    developper.setColor(gf::Color::White);
+    developper.setPosition(coords.getRelativePoint({ 0.1f, 0.4f}));
+    developper.setAnchor(gf::Anchor::Center);
+
+    gf::Text sound("", m_font, subtitleCharacterSize);
+    sound.setParagraphWidth(20.0f);
+    sound.setAlignment(gf::Alignment::Justify);
+    sound.setString("Sound:"
+                         " Antho ");
+    sound.setColor(gf::Color::White);
+    sound.setPosition(coords.getRelativePoint({ 0.4f, 0.4f}));
+    sound.setAnchor(gf::Anchor::Center);
+
+
+    gf::Text art("", m_font, subtitleCharacterSize);
+    art.setParagraphWidth(20.0f);
+    art.setAlignment(gf::Alignment::Justify);
+    art.setString("Art:"
+                         " Bovinbleu ");
+    art.setColor(gf::Color::White);
+    art.setPosition(coords.getRelativePoint({ 0.7f, 0.4f}));
+    art.setAnchor(gf::Anchor::Center);
+
+    target.draw(developper, states);
+    target.draw(sound, states);
+    target.draw(art, states);
+    target.draw(git, states);
   }
 
 }
