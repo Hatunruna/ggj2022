8314d4ec50e248828c7c0c7f8cd3bb916e49812e
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-02-15 22:55:43 +0100

    add two levels

--- code/bits/GameRaw.cc
+++ code/bits/GameRaw.cc
@@ -4,7 +4,7 @@ namespace hg {
 
   GameRaw::GameRaw(gf::AssetManager& assets)
   {
-    for (auto & path : { "level00.tmx", "level01.tmx" }) {
+    for (auto & path : { "level04.tmx", "level01.tmx", "level00.tmx", "level02.tmx" }) {
       gf::TmxLayers tmx;
       tmx.loadFromFile(assets.getAbsolutePath(path));
       levels.push_back(std::move(tmx));
